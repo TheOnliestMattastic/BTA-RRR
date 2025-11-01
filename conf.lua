dbf42a779376b8dda99e6abac07b9f68e380f0f7
Matthew P. Chicano <matthew.poole485@gmail.com>
2025-11-01 17:49:19 -0400

    Set minimum window size and disable auto-sizing

--- conf.lua
+++ conf.lua
@@ -26,8 +26,8 @@ function love.conf(t)
 	t.window.height         = 896
 	t.window.borderless     = false
 	t.window.resizable      = true
-	t.window.minwidth       = 1
-	t.window.minheight      = 1
+	t.window.minwidth       = 1024
+	t.window.minheight      = 896
 	t.window.fullscreen     = false
 	t.window.fullscreentype = "desktop"
 	t.window.vsync          = 1
--- core/ui.lua
+++ core/ui.lua
@@ -3,12 +3,11 @@ local Slab = require "lib.Slab.Slab"
 local ui = {}
 local options = {
 	ConstrainPosition = false,
+	AutoSizeWindow = false,
 	X = 0,
 	Y = 0,
 	W = winWidth,
 	H = winHeight,
-	ContentW = winWidth,
-	ContentH = winHeight
 }
 
 function ui.initialize(args)
@@ -16,6 +15,8 @@ function ui.initialize(args)
 end
 
 function ui.update(dt)
+    options.W = winWidth
+    options.H = winHeight
     Slab.Update(dt)
     Slab.BeginWindow('Background', options)
     Slab.Text("Hello")
