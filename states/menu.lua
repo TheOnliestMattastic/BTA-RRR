64502801370c4cb5a3835a9419f0e20c86da827d
Matthew P. Chicano <matthew.poole485@gmail.com>
2025-11-02 02:10:28 -0500

    Refactor menu state to use Slab UI elements

--- states/menu.lua
+++ states/menu.lua
@@ -7,21 +7,34 @@ registry:loadUI()
 
 function menu.load(args)
 	Slab.Initialize(args)
-
 end
 
 function menu.update(dt)
+	Slab.Update(dt)
+
+	local winWidth, winHeight = love.graphics.getDimensions()
 	local window = {
 		AutoSizeWindow = false,
 		X = 0,
 		Y = 0,
 		W = winWidth,
 		H = winHeight,
-		BgColor = {.3, .4, .4},
 		ConstrainPosition = true,
+		BgColor = {0.3, 0.4, 0.4},
 	}
+	Slab.BeginWindow("StartMenu", window)
+
+	local font = {
+		med = love.graphics.newFont("assets/fonts/alagard.ttf", 48),
+		large = love.graphics.newFont("assets/fonts/alagard.ttf", 96),
+	}
+	Slab.PushFont(font.large)
+
 	local textX = winWidth / 2 - (Slab.GetTextWidth("Battle Tactics Arena") / 2)
 	local textY = winHeight / 4
+	Slab.SetCursorPos(textX, textY)
+	Slab.Text("Battle Tactics Arena")
+
 	local button = {
 		Path = "assets/sprites/ui/button.png",
 		W = winWidth / 2,
@@ -32,14 +45,9 @@ function menu.update(dt)
 		SubH = 8,
 	}
 	local buttonX = winWidth / 2 - button.W / 2
-	local buttonY = 3 * winHeight / 4 - button.H / 2
+	local buttonY = 3 * winHeight / 4 - button.H / 2	Slab.SetCursorPos(buttonX, buttonY)
+	Slab.Image("Play", button)
 
-	Slab.Update(dt)
-	Slab.BeginWindow("StartMenu", window)
-	Slab.SetCursorPos(textX, textY)
-	Slab.Text("Battle Tactics Arena")
-	Slab.SetCursorPos(buttonX, buttonY)
-	Slab.Image("Start", button)
 	btnState = Slab.IsControlHovered() and 16 or 0
 	if Slab.IsMouseDown(1) and Slab.IsControlHovered() then
 		btnState = 48
@@ -47,12 +55,15 @@ function menu.update(dt)
 	if Slab.IsMouseReleased(1) and Slab.IsControlHovered() then
 		States.switch("game")
 	end
-	local textW = Slab.GetTextWidth("Start")
-	local textH = Slab.GetStyle().Font:getHeight()
+
+	Slab.PushFont(font.med)
+	local textW = Slab.GetTextWidth("Play")
+	local textH = font.med:getHeight()
 	local textX = buttonX + (button.W - textW) / 2
-	local textY = buttonY + (button.H - textH) / 2
+	local textY = buttonY + (button.H - textH) / 2 + (btnState == 48 and 32 or 0)
 	Slab.SetCursorPos(textX, textY)
-	Slab.Text("Start")
+	Slab.Text("Play")
+	Slab.PopFont()
 	Slab.EndWindow()
 end
 
