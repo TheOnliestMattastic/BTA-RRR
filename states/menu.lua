f879804df6ad2924494d43c6d4051f6bfa68f9f6
Matthew P. Chicano <matthew.poole485@gmail.com>
2025-11-02 01:30:20 -0400

    Refactor menu button image loading and rendering

--- states/menu.lua
+++ states/menu.lua
@@ -5,9 +5,6 @@ local menu = {}
 local registry = UIRegistry.new()
 registry:loadUI()
 
-local button = love.graphics.newImage("assets/sprites/ui/button.png")
-
-
 function menu.load(args)
 	Slab.Initialize(args)
 
@@ -25,12 +22,11 @@ function menu.update(dt)
 	}
 	local textX = winWidth / 2 - (Slab.GetTextWidth("Battle Tactics Arena") / 2)
 	local textY = winHeight / 4
-	local isHovered = btnState and 16 or 0
 	local button = {
-		Image = button,
+		Path = "assets/sprites/ui/button.png",
 		W = winWidth / 2,
 		H = winHeight / 4,
-		SubX = isHovered,
+		SubX = btnState,
 		SubY = 0,
 		SubW = 16,
 		SubH = 8,
@@ -38,17 +34,25 @@ function menu.update(dt)
 	local buttonX = winWidth / 2 - button.W / 2
 	local buttonY = 3 * winHeight / 4 - button.H / 2
 
-
 	Slab.Update(dt)
 	Slab.BeginWindow("StartMenu", window)
 	Slab.SetCursorPos(textX, textY)
 	Slab.Text("Battle Tactics Arena")
 	Slab.SetCursorPos(buttonX, buttonY)
 	Slab.Image("Start", button)
-	btnState = Slab.IsControlHovered()
-	if Slab.IsControlClicked() then
+	btnState = Slab.IsControlHovered() and 16 or 0
+	if Slab.IsMouseDown(1) and Slab.IsControlHovered() then
+		btnState = 48
+	end
+	if Slab.IsMouseReleased(1) and Slab.IsControlHovered() then
 		States.switch("game")
 	end
+	local textW = Slab.GetTextWidth("Start")
+	local textH = Slab.GetStyle().Font:getHeight()
+	local textX = buttonX + (button.W - textW) / 2
+	local textY = buttonY + (button.H - textH) / 2
+	Slab.SetCursorPos(textX, textY)
+	Slab.Text("Start")
 	Slab.EndWindow()
 end
 
